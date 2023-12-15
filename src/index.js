8286fda272b2f4e21b5f94af95642832e59e79e2
kessie <kayp3477@gmail.com>
2023-12-15 22:47:11 +0000

    Imported and rendered components in their respective routes

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
+
+import ErrorPage from './error-page';
+import HomePage from './HomePage';
+import About from './About';
+import SearchBook from './SearchBook';
+import Contact from './Contact';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/',
+        element: <HomePage />,
+      },
+      {
+        path: '/about',
+        element: <About />,
+      },
+      {
+        path: '/contact',
+        element: <Contact />,
+      },
+      {
+        path: '/search-book',
+        element: <SearchBook />,
+      },
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+);
