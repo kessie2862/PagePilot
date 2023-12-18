c690821f91b1b1fdac0b1664ef0bc3a28525502e
kessie <kayp3477@gmail.com>
2023-12-18 22:05:03 +0000

    Fix linter error

--- src/routes/root.js
+++ src/routes/root.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { Outlet, NavLink, Link } from 'react-router-dom';
 import { Dialog } from '@headlessui/react';
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
+import '../index.css';
 
 const navigation = [
   {
