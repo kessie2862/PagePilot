b42347a94fc5ed487e577fa7112e5d6550d9cdde
kessie <kayp3477@gmail.com>
2023-12-28 19:38:45 +0000

    Fix linter error

--- src/HomeBg.js
+++ src/HomeBg.js
@@ -19,7 +19,7 @@ function HomeBg() {
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div className="text-center">
             <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-              Find the boks you are looking for anywhere
+              Find the books you are looking for anywhere
             </h1>
             <p className="mt-6 text-lg leading-8 text-gray-600">
               Search for books anywhere, you don&apos;t need to know the name of
