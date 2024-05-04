2f481dcd3a831a411a9596b29ce065b968897798
kessie <kayp3477@gmail.com>
2024-05-04 20:06:50 +0000

    Removed unnecessary console.log

--- src/Modal.js
+++ src/Modal.js
@@ -35,9 +35,7 @@ const Modal = ({ show, item, onClose }) => {
                 className="box-content rounded-none border-none hover:no-underline hover:opacity-75 focus:opacity-100 focus:shadow-none focus:outline-none"
                 onClick={onClose}
                 aria-label="Close Modal"
-              >
-                {/* <FontAwesomeIcon icon="fas fa-window-close" /> */}
-              </button>
+              />
             </TEModalHeader>
             <TEModalBody>
               <div className="inner-box">
@@ -49,9 +47,6 @@ const Modal = ({ show, item, onClose }) => {
                     <span>{item.volumeInfo.publishedDate}</span>
                   </h4>
                   <br />
-                  <a href={item.volumeInfo.previewLink}>
-                    <button type="button">More</button>
-                  </a>
                 </div>
               </div>
               <h4 className="description">{item.volumeInfo.description}</h4>
@@ -67,13 +62,15 @@ const Modal = ({ show, item, onClose }) => {
                 </button>
               </TERipple>
               <TERipple rippleColor="light">
-                <button
-                  type="button"
+                <a
+                  href={item.volumeInfo.previewLink}
+                  target="_blank"
+                  rel="noreferrer noopener"
                   className="ml-1 inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
                   onClick={onClose}
                 >
-                  Understood
-                </button>
+                  More
+                </a>
               </TERipple>
             </TEModalFooter>
           </TEModalContent>
