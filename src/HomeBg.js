221ed4d6334087b8608df30da9c5416f3d507ed8
kessie <kayp3477@gmail.com>
2024-05-05 16:37:50 +0000

    Fix linter error

--- src/HomeBg.js
+++ src/HomeBg.js
@@ -31,7 +31,9 @@ function HomeBg() {
                 href="/search-book"
                 className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                SEARCH BOOK <span aria-hidden="true">▶▶</span>
+                SEARCH BOOK
+                {' '}
+                <span aria-hidden="true">▶▶</span>
               </a>
             </div>
           </div>
