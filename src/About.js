54959c30e05aa2e1e581747fd12525b2c750d226
kessie <kayp3477@gmail.com>
2024-05-06 15:07:46 +0000

    Add applications name

--- src/About.js
+++ src/About.js
@@ -7,18 +7,18 @@ function About() {
     <div className="flex flex-col items-center justify-center h-screen">
       <Root />
       <div className="mt-8 max-w-lg text-center">
-        <h1 className="text-3xl font-bold mb-4">About Book Search App</h1>
+        <h1 className="text-3xl font-bold mb-4">About PagePilot App</h1>
         <p className="text-lg mb-4">
-          Welcome to the Book Search App! This application allows you to search
+          Welcome to the PagePilot App! This application allows you to search
           for your favorite books and explore a vast collection of literature.
         </p>
         <p className="text-lg mb-4">
-          With Book Search, you can discover new authors, explore different
+          With PagePilot, you can discover new authors, explore different
           genres, and find that special book you&apos;ve been looking for.
         </p>
         <p className="text-lg mb-4">
           Whether you&apos;re an avid reader, a student conducting research, or
-          simply someone who loves books, Book Search is your go-to destination
+          simply someone who loves books, PagePilot is your go-to destination
           for all things literature.
         </p>
         <p className="text-lg">Start searching for your next great read now!</p>
